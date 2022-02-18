0f8b91ec89611717b3d8db1dac5a853f8fbea778
Maldus512 <mattia512maldini@gmail.com>
2022-02-18 16:57:51 +0100

    Ora/data e tastiera

--- main/view/pages/page_program_name.c
+++ main/view/pages/page_program_name.c
@@ -1,6 +1,11 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
 #include "lvgl/lvgl.h"
+#include "src/lv_core/lv_obj.h"
+#include "src/lv_misc/lv_task.h"
+#include "src/lv_objx/lv_label.h"
+#include "view/styles.h"
 #include "view/view.h"
 #include "view/images/legacy.h"
 #include "view/view_types.h"
@@ -10,26 +15,87 @@
 #include "view/common.h"
 #include "view/intl/intl.h"
 #include "utils/utils.h"
+#include "esp_log.h"
 
 
 // enum { };
 
-
-struct page_data {
-    name_t names[MAX_LINGUE];
+enum {
+    TIMEOUT_TIMER_ID,
 };
 
+
+struct page_data {
+    size_t    cindex;
+    size_t    rindex;
+    lv_obj_t *labels[4][10];
+
+    int flag;
+
+    lv_task_t *task;
+    name_t     names[MAX_LINGUE];
+};
+
+static void labels_set_text(struct page_data *data);
 
 
 static void *create_page(model_t *pmodel, void *extra) {
     struct page_data *pdata = malloc(sizeof(struct page_data));
+    pdata->task             = view_register_periodic_task(500UL, LV_TASK_PRIO_OFF, TIMEOUT_TIMER_ID);
     return pdata;
 }
 
 
+
 static void open_page(model_t *pmodel, void *args) {
-    struct page_data *pdata = args;
-    (void)pdata;
+    struct page_data *data = args;
+
+    lv_task_set_prio(data->task, LV_TASK_PRIO_MID);
+
+    data->rindex = 0;
+    data->cindex = 0;
+
+    data->flag = 1;
+
+    lv_obj_t *labels[4][10];
+
+    for (int i = 0; i < 4; i++) {
+        labels[i][0] = lv_label_create(lv_scr_act(), NULL);
+        for (int j = 1; j < 10; j++) {
+            if ((i == 0) || (i == 1 && j != 9) || (i == 2 && j <= 6) || (i == 3 && j <= 3)) {
+                labels[i][j] = lv_label_create(lv_scr_act(), NULL);
+            } else {
+                labels[i][j] = NULL;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 10; j++) {
+            data->labels[i][j] = labels[i][j];
+        }
+    }
+
+    labels_set_text(data);
+    lv_obj_align(data->labels[2][0], NULL, LV_ALIGN_CENTER, -30, 10);
+    lv_obj_align(data->labels[1][0], NULL, LV_ALIGN_CENTER, -37, 0);
+    lv_obj_align(data->labels[0][0], NULL, LV_ALIGN_CENTER, -47, -10);
+    lv_obj_align(data->labels[3][0], NULL, LV_ALIGN_IN_BOTTOM_LEFT, 0, 0);
+    lv_label_set_body_draw(labels[0][0], 1);
+    lv_label_set_body_draw(labels[1][0], 1);
+    lv_label_set_body_draw(labels[2][0], 1);
+    lv_label_set_body_draw(labels[3][0], 1);
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 1; j < 10; j++) {
+            if (labels[i][j] == NULL) {
+                continue;
+            }
+            lv_obj_align(data->labels[i][j], data->labels[i][j - 1], LV_ALIGN_OUT_RIGHT_MID, 3, 0);
+            lv_label_set_body_draw(labels[i][j], 1);
+        }
+    }
 }
 
 
@@ -47,14 +113,118 @@ static view_message_t process_page_event(model_t *pmodel, void *arg, view_event_
                         msg.vmsg.code = VIEW_PAGE_COMMAND_CODE_BACK;
                         break;
                     }
-                    default:
+                    case BUTTON_DESTRA: {
+                        switch (data->rindex) {
+                            case (0):
+                                if (data->cindex < 9) {
+                                    data->cindex++;
+                                } else if (data->cindex == 9) {
+                                    data->cindex = 0;
+                                    data->rindex++;
+                                }
+                                break;
+                            case (1):
+                                if (data->cindex < 8) {
+                                    data->cindex++;
+                                } else if (data->cindex == 8) {
+                                    data->cindex = 0;
+                                    data->rindex++;
+                                }
+                                break;
+                            case (2):
+                                if (data->cindex < 6) {
+                                    data->cindex++;
+                                } else if (data->cindex == 6) {
+                                    data->cindex = 0;
+                                    data->rindex++;
+                                }
+                                break;
+                            case (3):
+                                if (data->cindex < 3) {
+                                    data->cindex++;
+                                }
+                                break;
+                        }
                         break;
+                    }
+
+                    case BUTTON_CALDO: {
+                        switch (data->rindex) {
+                            case (0):
+                                if (data->cindex > 0) {
+                                    data->cindex--;
+                                }
+                                break;
+                            case (1):
+                                if (data->cindex > 0) {
+                                    data->cindex--;
+                                } else if (data->cindex == 0) {
+                                    data->cindex = 9;
+                                    data->rindex--;
+                                }
+                                break;
+                            case (2):
+                                if (data->cindex > 0) {
+                                    data->cindex--;
+                                } else if (data->cindex == 0) {
+                                    data->cindex = 8;
+                                    data->rindex--;
+                                }
+                                break;
+                            case (3):
+                                if (data->cindex > 0) {
+                                    data->cindex--;
+                                } else if (data->cindex == 0) {
+                                    data->cindex = 6;
+                                    data->rindex--;
+                                }
+                                break;
+
+                            default:
+                                break;
+                        }
+                        break;
+                    } break;
+                    case BUTTON_FREDDO: {
+                        if (data->rindex < 3) {
+                            data->rindex++;
+                            if (data->labels[data->rindex][data->cindex] == NULL) {
+                                do {
+                                    data->cindex--;
+                                } while (data->labels[data->rindex][data->cindex] == NULL);
+                            }
+                        }
+                    } break;
+                    case BUTTON_PIU: {
+                        if (data->rindex > 0) {
+                            data->rindex--;
+                            if (data->labels[data->rindex][data->cindex] == NULL) {
+                                do {
+                                    data->cindex--;
+                                } while (data->labels[data->rindex][data->cindex] == NULL);
+                            }
+                        }
+                    } break;
                 }
             }
-        } break;
+        }
+        case VIEW_EVENT_CODE_TIMER: {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (data->labels[i][j] == NULL) {
+                        continue;
+                    }
+                    if (i == data->rindex && j == data->cindex) {
+                        lv_obj_set_style(data->labels[i][j], &style_label_reverse);
+                    } else {
+                        lv_obj_set_style(data->labels[i][j], &style_label_normal);
+                    }
+                }
+            }
+            lv_task_reset(data->task);
+        }
 
-        case VIEW_EVENT_CODE_TIMER:
-            break;
+        break;
 
         default:
             break;
@@ -65,13 +235,14 @@ static view_message_t process_page_event(model_t *pmodel, void *arg, view_event_
 
 static void close_page(void *arg) {
     struct page_data *data = arg;
-    (void)data;
+    lv_task_set_prio(data->task, LV_TASK_PRIO_OFF);
     lv_obj_clean(lv_scr_act());
 }
 
 
 static void destroy_page(void *arg, void *extra) {
     struct page_data *data = arg;
+    lv_task_del(data->task);
     free(data);
     free(extra);
 }
@@ -84,3 +255,39 @@ const pman_page_t page_program_name = {
     .close         = close_page,
     .destroy       = destroy_page,
 };
+
+static void labels_set_text(struct page_data *data) {
+    lv_label_set_text(data->labels[0][0], "Q");
+    lv_label_set_text(data->labels[0][1], "W");
+    lv_label_set_text(data->labels[0][2], "E");
+    lv_label_set_text(data->labels[0][3], "R");
+    lv_label_set_text(data->labels[0][4], "T");
+    lv_label_set_text(data->labels[0][5], "Y");
+    lv_label_set_text(data->labels[0][6], "U");
+    lv_label_set_text(data->labels[0][7], "I");
+    lv_label_set_text(data->labels[0][8], "O");
+    lv_label_set_text(data->labels[0][9], "P");
+
+    lv_label_set_text(data->labels[1][0], "A");
+    lv_label_set_text(data->labels[1][1], "S");
+    lv_label_set_text(data->labels[1][2], "D");
+    lv_label_set_text(data->labels[1][3], "F");
+    lv_label_set_text(data->labels[1][4], "G");
+    lv_label_set_text(data->labels[1][5], "H");
+    lv_label_set_text(data->labels[1][6], "J");
+    lv_label_set_text(data->labels[1][7], "K");
+    lv_label_set_text(data->labels[1][8], "L");
+
+    lv_label_set_text(data->labels[2][0], "Z");
+    lv_label_set_text(data->labels[2][1], "X");
+    lv_label_set_text(data->labels[2][2], "C");
+    lv_label_set_text(data->labels[2][3], "V");
+    lv_label_set_text(data->labels[2][4], "B");
+    lv_label_set_text(data->labels[2][5], "N");
+    lv_label_set_text(data->labels[2][6], "M");
+
+    lv_label_set_text(data->labels[3][0], "[EN]");
+    lv_label_set_text(data->labels[3][1], "space");
+    lv_label_set_text(data->labels[3][2], "del");
+    lv_label_set_text(data->labels[3][3], "azz");
+}
--- main/view/pages/page_set_datetime.c
+++ main/view/pages/page_set_datetime.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include "src/lv_core/lv_obj.h"
 #include "src/lv_misc/lv_task.h"
 #include "src/lv_objx/lv_label.h"
+#include "view/styles.h"
 #include "view/view.h"
 #include "gel/pagemanager/page_manager.h"
 #include "model/model.h"
@@ -12,6 +15,11 @@
 #include "peripherals/keyboard.h"
 #include "controller/controller.h"
 #include "utils/utils.h"
+#include "view/intl/intl.h"
+#include "esp_log.h"
+
+
+#define STRING_MAX_LEN 255
 
 enum {
     TIMEOUT_TIMER_ID,
@@ -21,15 +29,11 @@ enum {
 struct page_data {
     lv_obj_t *lora;
     lv_obj_t *ldata;
+    lv_obj_t *lday;
 
     int flag;
 
-    uint8_t hour;
-    uint8_t min;
-    uint8_t sec;
-    uint8_t day;
-    uint8_t month;
-    uint8_t year;
+    struct tm datetime;
 
     size_t     index;
     char       prec;
@@ -37,8 +41,10 @@ struct page_data {
 };
 
 
-static void timedata_update(struct page_data *data);
-static void time_op(struct page_data *data, int op);
+static void        time_op(model_t *pmodel, struct page_data *data, int op);
+static const char *get_string_day(model_t *pmodel, struct page_data *data);
+static void        timedata_update(model_t *pmodel, struct page_data *data);
+//static int         check_date(struct tm tm);
 
 
 static void *create_page(model_t *model, void *extra) {
@@ -73,18 +79,20 @@ static void open_page(model_t *model, void *args) {
     lv_obj_align(ldata, NULL, LV_ALIGN_IN_TOP_RIGHT, 0, 30);
     data->ldata = ldata;
 
+    lv_obj_t *lday = lv_label_create(lv_scr_act(), NULL);
+    lv_label_set_body_draw(lday, 1);
+    lv_obj_set_auto_realign(lday, 1);
+    lv_obj_align(lday, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, -10);
+    data->lday = lday;
+
     data->index = 0;
 
-    struct tm time;
-    utils_get_sys_time(&time);
-    data->hour  = time.tm_hour;
-    data->min   = time.tm_min;
-    data->sec   = time.tm_sec;
-    data->day   = time.tm_mday;
-    data->month = time.tm_mon + 1;
-    data->year  = time.tm_year - 100;
-    lv_label_set_text_fmt(data->lora, ":%02i:%02i:%02i", data->hour, data->min, data->sec);
-    lv_label_set_text_fmt(data->ldata, ":%02i/%02i/%02i", data->day, data->month, data->year);
+    utils_get_sys_time(&data->datetime);
+    lv_label_set_text_fmt(data->lora, ":%02i:%02i:%02i", data->datetime.tm_hour, data->datetime.tm_min,
+                          data->datetime.tm_sec);
+    lv_label_set_text_fmt(data->ldata, ":%02i/%02i/%02i", data->datetime.tm_mday, data->datetime.tm_mon + 1,
+                          data->datetime.tm_year - 100);
+    lv_label_set_text(data->lday, get_string_day(model, data));
 }
 
 
@@ -103,21 +111,21 @@ static view_message_t process_page_event(model_t *model, void *args, pman_event_
                         if (data->index < 12) {
                             data->index++;
                             data->flag = 1;
-                            timedata_update(data);
+                            timedata_update(model, data);
                         }
                         break;
                     case BUTTON_CALDO:     // skip sinistra
                         if (data->index > 0) {
                             data->index--;
                             data->flag = 1;
-                            timedata_update(data);
+                            timedata_update(model, data);
                         }
                         break;
                     case BUTTON_PIU:     // più
-                        time_op(data, 1);
+                        time_op(model, data, 1);
                         break;
                     case BUTTON_FREDDO:     // meno
-                        time_op(data, -1);
+                        time_op(model, data, -1);
                         break;
                     default:
                         break;
@@ -127,7 +135,7 @@ static view_message_t process_page_event(model_t *model, void *args, pman_event_
         }
 
         case VIEW_EVENT_CODE_TIMER: {
-            timedata_update(data);
+            timedata_update(model, data);
         } break;
         default:
             break;
@@ -153,77 +161,151 @@ static void destroy_page(void *arg, void *extra) {
 
 
 
-static void time_op(struct page_data *data, int op) {
+static void time_op(model_t *pmodel, struct page_data *data, int op) {
     switch (data->index) {
         case (0): {
-            int newhour = data->hour + op * 10;
+            int newhour = data->datetime.tm_hour + op * 10;
             if (newhour >= 0 && newhour < 24) {
-                data->hour = newhour;
+                data->datetime.tm_hour = newhour;
             }
             break;
         }
 
         case (1): {
-            int newhour = data->hour + op;
+            int newhour = data->datetime.tm_hour + op;
             if (newhour >= 0 && newhour < 24) {
-                data->hour = newhour;
+                data->datetime.tm_hour = newhour;
             }
             break;
         }
 
         case (2): {
-            int newmin = data->min + op * 10;
+            int newmin = data->datetime.tm_min + op * 10;
             if (newmin >= 0 && newmin < 60) {
-                data->min = newmin;
+                data->datetime.tm_min = newmin;
             }
             break;
         }
         case (3): {
-            int newmin = data->min + op;
+            int newmin = data->datetime.tm_min + op;
             if (newmin >= 0 && newmin < 60) {
-                data->min = newmin;
+                data->datetime.tm_min = newmin;
             }
             break;
         }
         case (4): {
-            int newsec = data->sec + op * 10;
+            int newsec = data->datetime.tm_sec + op * 10;
             if (newsec >= 0 && newsec < 60) {
-                data->sec = newsec;
+                data->datetime.tm_sec = newsec;
             }
             break;
         }
         case (5): {
-            int newsec = data->sec + op;
+            int newsec = data->datetime.tm_sec + op;
             if (newsec >= 0 && newsec < 60) {
-                data->sec = newsec;
+                data->datetime.tm_sec = newsec;
             }
+            break;
+        }
+        case (6): {
+            /*int oldday             = data->datetime.tm_mday;
+            data->datetime.tm_mday = oldday + op * 10;
+            if (!check_date(data->datetime)) {
+                data->datetime.tm_mday = oldday;
+            }*/
+            data->datetime.tm_mday = data->datetime.tm_mday + op * 10;
+            mktime(&data->datetime);
+            break;
+        }
+        case (7): {
+            /*int oldday             = data->datetime.tm_mday;
+            data->datetime.tm_mday = oldday + op;
+            if (!check_date(data->datetime)) {
+                data->datetime.tm_mday = oldday;
+            }*/
+            data->datetime.tm_mday = data->datetime.tm_mday + op;
+            mktime(&data->datetime);
+            break;
+        }
+        case (8): {
+            /*int oldday             = data->datetime.tm_mday;
+            data->datetime.tm_mday = oldday + op;
+            if (!check_date(data->datetime)) {
+                data->datetime.tm_mday = oldday;
+            }*/
+            data->datetime.tm_mon = data->datetime.tm_mon + op * 10;
+            mktime(&data->datetime);
+            break;
+        }
+        case (9): {
+            /*int oldday             = data->datetime.tm_mday;
+            data->datetime.tm_mday = oldday + op;
+            if (!check_date(data->datetime)) {
+                data->datetime.tm_mday = oldday;
+            }*/
+            data->datetime.tm_mon = data->datetime.tm_mon + op;
+            mktime(&data->datetime);
+            break;
+        }
+        case (10): {
+            int newyear = data->datetime.tm_year + op * 10;
+            if (newyear >= 100 && newyear < 200) {
+                data->datetime.tm_year = newyear;
+                mktime(&data->datetime);
+            }
+            break;
+        }
+        case (11): {
+            int newyear = data->datetime.tm_year + op;
+            if (newyear >= 100 && newyear < 200) {
+                data->datetime.tm_year = newyear;
+                mktime(&data->datetime);
+            }
+            break;
+        }
+        case (12): {
+            /*int newday = data->datetime.tm_wday + op;
+            if (newday >= 0 && newday <= 6) {
+                data->datetime.tm_wday = newday;
+            }*/
+            data->datetime.tm_wday = data->datetime.tm_wday + op;
+            mktime(&data->datetime);
             break;
         }
     }
 
     data->flag = 0;
-    timedata_update(data);
+    timedata_update(pmodel, data);
 }
 
 
-static void timedata_update(struct page_data *data) {
+static void timedata_update(model_t *pmodel, struct page_data *data) {
 
     char timedata[12];
-    timedata[0]  = (data->hour / 10) + 48;
-    timedata[1]  = (data->hour % 10) + 48;
-    timedata[2]  = (data->min / 10) + 48;
-    timedata[3]  = (data->min % 10) + 48;
-    timedata[4]  = (data->sec / 10) + 48;
-    timedata[5]  = (data->sec % 10) + 48;
-    timedata[6]  = (data->day / 10) + 48;
-    timedata[7]  = (data->day % 10) + 48;
-    timedata[8]  = (data->month / 10) + 48;
-    timedata[9]  = (data->month % 10) + 48;
-    timedata[10] = (data->year / 10) + 48;
-    timedata[11] = (data->year % 10) + 48;
+    timedata[0]  = (data->datetime.tm_hour / 10) + 48;
+    timedata[1]  = (data->datetime.tm_hour % 10) + 48;
+    timedata[2]  = (data->datetime.tm_min / 10) + 48;
+    timedata[3]  = (data->datetime.tm_min % 10) + 48;
+    timedata[4]  = (data->datetime.tm_sec / 10) + 48;
+    timedata[5]  = (data->datetime.tm_sec % 10) + 48;
+    timedata[6]  = (data->datetime.tm_mday / 10) + 48;
+    timedata[7]  = (data->datetime.tm_mday % 10) + 48;
+    timedata[8]  = ((data->datetime.tm_mon + 1) / 10) + 48;
+    timedata[9]  = ((data->datetime.tm_mon + 1) % 10) + 48;
+    timedata[10] = ((data->datetime.tm_year - 100) / 10) + 48;
+    timedata[11] = ((data->datetime.tm_year - 100) % 10) + 48;
+
+    char weekday[STRING_MAX_LEN] = {0};
+    memcpy(weekday, get_string_day(pmodel, data), strlen(get_string_day(pmodel, data)));
 
     if (data->flag) {
-        timedata[data->index] = '_';
+        if (data->index < 12) {
+            timedata[data->index] = '_';
+        } else if (data->index == 12) {
+            lv_obj_set_style(data->lday, &style_label_reverse);
+        }
+    } else {
+        lv_obj_set_style(data->lday, &style_label_normal);
     }
     data->flag = !data->flag;
 
@@ -231,12 +313,31 @@ static void timedata_update(struct page_data *data) {
                           timedata[4], timedata[5]);
     lv_label_set_text_fmt(data->ldata, ":%c%c/%c%c/%c%c", timedata[6], timedata[7], timedata[8], timedata[9],
                           timedata[10], timedata[11]);
+    lv_label_set_text(data->lday, weekday);
 
     lv_task_reset(data->task);
 }
 
-
-
+static const char *get_string_day(model_t *pmodel, struct page_data *data) {
+    switch (data->datetime.tm_wday) {
+        case (0):
+            return view_intl_get_string(pmodel, STRINGS_DOMENICA);
+        case (1):
+            return view_intl_get_string(pmodel, STRINGS_LUNEDI);
+        case (2):
+            return view_intl_get_string(pmodel, STRINGS_MARTEDI);
+        case (3):
+            return view_intl_get_string(pmodel, STRINGS_MERCOLEDI);
+        case (4):
+            return view_intl_get_string(pmodel, STRINGS_GIOVEDI);
+        case (5):
+            return view_intl_get_string(pmodel, STRINGS_VENERDI);
+        case (6):
+            return view_intl_get_string(pmodel, STRINGS_SABATO);
+        default:
+            return 0;
+    }
+}
 
 const pman_page_t page_set_datetime = {
     .create        = create_page,
@@ -245,3 +346,17 @@ const pman_page_t page_set_datetime = {
     .close         = close_page,
     .destroy       = destroy_page,
 };
+
+/*
+static int check_date(struct tm tm) {
+    struct tm res  = tm;
+    time_t    time = mktime(&res);
+    localtime(&time);
+    ESP_LOGI("x", "%i %i %i %i %i %i", tm.tm_mday, res.tm_mday, tm.tm_mon, res.tm_mon, tm.tm_year, res.tm_year);
+    if (time == -1 || res.tm_mday != tm.tm_mday || res.tm_mon != tm.tm_mon || res.tm_year != tm.tm_year) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+*/
